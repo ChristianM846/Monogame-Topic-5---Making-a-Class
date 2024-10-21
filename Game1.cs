dc24efc830ddb20813a8e986b78d59ae054511eb
Christian Moyes <339463846@GoTVDSB.ca>
2024-10-21 09:17:37 -0400

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
+namespace Monogame_Topic_5___Making_a_Class
+{
+    public class Game1 : Game
+    {
+        //Christian Moyes
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        MouseState mouseState;
+
+        Random genrator = new Random();
+
+        Texture2D titleTexture;
+        Texture2D backgroundTexture;
+        Texture2D endTexture;
+        Texture2D marioTexture;
+
+        Rectangle windowRect;
+
+        List<Texture2D> ghostTextures;
+
+        enum Screen
+        {
+            Title,
+            House,
+            End
+        }
+
+        Screen screen;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            _graphics.PreferredBackBufferHeight = 600;
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.ApplyChanges();
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            genrator = new Random();
+            ghostTextures = new List<Texture2D>();
+
+            windowRect = new Rectangle(0, 0, 800, 600);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            marioTexture = Content.Load<Texture2D>("Images/mario");
+            titleTexture = Content.Load<Texture2D>("Images/haunted-title");
+            backgroundTexture = Content.Load<Texture2D>("Images/haunted-background");
+            endTexture = Content.Load<Texture2D>("Images/haunted-end-screen");
+
+            ghostTextures.Add(Content.Load<Texture2D>("Images/boo-stopped"));
+
+            for (int i = 1; i <= 8; i++)
+            {
+                ghostTextures.Add(Content.Load<Texture2D>($"Images/boo-move-{i}"));
+            }
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            mouseState = Mouse.GetState();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(backgroundTexture, windowRect, Color.White);
+
+
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Monogame_Topic_5___Making_a_Class.Game1();
+game.Run();
